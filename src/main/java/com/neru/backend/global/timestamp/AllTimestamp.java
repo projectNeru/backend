eec0f5366b6e298e098b65f3307e9e261b673857
youngju <ysy561356@naver.com>
2025-06-16 23:49:47 +0900

    feat : 공용 타임스탬프 추가

---
+++ src/main/java/com/neru/backend/global/timestamp/AllTimestamp.java
@@ -0,0 +1,36 @@
+package com.neru.backend.global.timestamp;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class AllTimestamp {
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        if (updatedAt == null) {
+            updatedAt = createdAt;
+        }
+    }
+
+    public void updateUpdatedAt() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
